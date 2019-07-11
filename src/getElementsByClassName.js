fc97ac11125bc46c22717a8751a4484d25059481
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-07-11 01:44:55 -0400

    Implements getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, currentNode = document.body, result = []) {
+  // base case, current node's childNode property has length 0
+    // return the current node
+  if (currentNode.childNodes.length === 0) {
+    return currentNode;
+  }
+  // recursive case, current node's childNodes property has length > 0
+  if (currentNode.childNodes.length > 0) {
+    // if current node is element & has className, push current node to result array
+    if (currentNode.nodeType === 1 && currentNode.classList.contains(className)) {
+      result.push(currentNode);
+    }
+    Array.from(currentNode.childNodes).forEach(node => {
+      // loop through childNodes list
+      // invoke recursion on each child node,
+      let value = getElementsByClassName(className, node, result);
+      // if returned value is an element with the target class, push node to result array
+      if (value.nodeType === 1 && value.classList.contains(className)) {
+        result.push(value);
+      }
+    });
+  }
+  // return current status of result array for this recursive depth
+  return result;
 };
